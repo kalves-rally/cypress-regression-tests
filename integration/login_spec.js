80dbf89c63a4cef1c76db8ab4396b4e6b48cb450
Alves <alvka02@alvka02mac1416.ca.com>
2018-01-30 16:13:08 -0700

    request

--- integration/login_spec.js
+++ integration/login_spec.js
@@ -1,16 +1,36 @@
-describe('Login Test', function(){
-    it('Visits Rally', function(){
-        cy
-          //the url is stored on cypress.json
-          .visit('/login.op')
-          .get('#j_username').type('test@rallydev.com').should("have.value", "test@rallydev.com")
-          .get('#j_password').type('Password').should("have.value", "Password")
-          .get('#login-button').click()
-          //.contains('Sign In').click()
-          .url().should('include', '/dashboard')
+// describe('Login Test', function(){
+//     it('Visits Rally', function(){
+//         cy
+//           //the url is stored on cypress.json
+//           .visit('/login.op')
+//           .get('#j_username').type('test@rallydev.com').should("have.value", "test@rallydev.com")
+//           .get('#j_password').type('Password').should("have.value", "Password")
+//           .get('#login-button').click()
+//           //.contains('Sign In').click()
+//           .url().should('include', '/dashboard')
+          
 
+//     })
+// })
+
+describe('Login Test', function(){
+   it('Visits Rally', function(){
+    cy.request({
+        method: 'POST',
+        url:'/login.op',
+        form: true,
+        body: {
+            username: 'test@rallydev.com',
+            password: 'Password'
+        }
+    }).then((resp) => {
+        expect(resp.status).to.eq(200)
+        console.log(resp);
     })
+    cy.getCookie('ZSESSIONID','VsqEZFcSiOuoA3S6dElM3gU5NQUWuleFAJBz64c').should('exist')
+   }) 
 })
+
 
 
 //visit a web page
